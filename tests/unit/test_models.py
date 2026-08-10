d648061b041e5fd94fd4c0f16aa19a776cafda32
fastgrandmother <roman34365@gmail.com>
2026-08-10 20:30:55 +0500

    Adjust allure report

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -3,84 +3,90 @@ from pydantic import ValidationError
 from app.models import TaskCreate
 import allure
 
-@allure.feature("Test models")
-@allure.story("Create a new valid task")
-@allure.severity(allure.severity_level.CRITICAL)
-def test_task_create_success():
-    with allure.step("Fill payload"):
+pytestmark = [allure.feature("Валидация Pydantic моделей")]
+
+@allure.story("Создание модели TaskCreate")
+class TestTaskCreateValidation:
+
+    @allure.title("Create a new valid task")
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_task_create_success(self):
         """Позитивный тест: валидные данные должны успешно проходить проверку"""
-        payload = {
-        "name": "Написать автотесты",
-        "description": "Покрыть unit-тестами слой валидации Pydantic",
-        "author": "Инженер поддержки"
-        }
+        with allure.step("Fill payload"):
 
-    with allure.step("Initializing the model"):
-        # Инициализурем модель
-        task = TaskCreate(**payload)
-    with allure.step("Check matching value the payload"):
-        # Проверяем, что Pydantic правильно сопоставил поля
-        assert task.name == payload["name"]
-        assert task.description == payload["description"]
-        assert task.author == payload["author"]
+            payload = {
+            "name": "Написать автотесты",
+            "description": "Покрыть unit-тестами слой валидации Pydantic",
+            "author": "Инженер поддержки"
+            }
 
-@allure.feature("Test models")
-@allure.story("Create a new task with invalid 'name'")
-@allure.severity(allure.severity_level.NORMAL)
-def test_task_create_name_too_long():
-    with allure.step("Fill invalid name"):
+        with allure.step("Initializing the model"):
+            # Инициализурем модель
+            task = TaskCreate(**payload)
+        with allure.step("Check matching value the payload"):
+            # Проверяем, что Pydantic правильно сопоставил поля
+            assert task.name == payload["name"]
+            assert task.description == payload["description"]
+            assert task.author == payload["author"]
+
+    @allure.title("Create a new task with invalid 'name'")
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_task_create_name_too_long(self):
         """Негативный тест: имя задачи ровно 101 символ (граница > 100)"""
-        invalid_name = "X" * 101  # Генерируем слишком длинную строку
+        with allure.step("Fill invalid name"):
+            invalid_name = "X" * 101  # Генерируем слишком длинную строку
 
-    with allure.step("Fill payload"):
-        payload = {
-        "name": invalid_name,
-        "description": "Тест граничных значений",
-        "author": "QA"
-        }
-    with allure.step("Check that the ValidationError is received"):
-        # Ожидаем, что Pydantic выбросит ошибку ValidationError
-        with pytest.raises(ValidationError) as exc_info:
-            TaskCreate(**payload)
+        with allure.step("Fill payload"):
+            payload = {
+            "name": invalid_name,
+            "description": "Тест граничных значений",
+            "author": "QA"
+            }
+        with allure.step("Check that the ValidationError is received"):
+            # Ожидаем, что Pydantic выбросит ошибку ValidationError
+            with pytest.raises(ValidationError) as exc_info:
+                TaskCreate(**payload)
 
-    with allure.step("Check that the ValidationError for 'name' value"):
-        # Проверяем, что ошибка локализована именно на поле 'name'
-        assert "name" in str(exc_info.value)
+        with allure.step("Check that the ValidationError for 'name' value"):
+            # Проверяем, что ошибка локализована именно на поле 'name'
+            errors = exc_info.value.errors()
+            assert errors[0]["loc"] == ("name",)
+            assert errors[0]["type"] == "string_too_long"
 
-@allure.feature("Test models")
-@allure.story("Create a new task with invalid 'author'")
-@allure.severity(allure.severity_level.NORMAL)
-def test_task_create_author_empty():
-    with allure.step("Fill payload with empty author"):
+    @allure.title("Create a new task with invalid 'author'")
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_task_create_author_empty(self):
         """негативный тест: пустая строка в авторе (min_leght=1)"""
-        payload = {
-        "name":"Валидная задача",
-        "description": "Описание",
-        "author": ""
-        }
+        with allure.step("Fill payload with empty author"):
+            payload = {
+            "name":"Валидная задача",
+            "description": "Описание",
+            "author": ""
+            }
 
-    with allure.step("Check that the ValidationError is received"):
-        with pytest.raises(ValidationError) as exc_info:
-            TaskCreate(**payload)
+        with allure.step("Check that the ValidationError is received"):
+            with pytest.raises(ValidationError) as exc_info:
+                TaskCreate(**payload)
 
-    with allure.step("Check that the ValidationError for 'author' value"):
-        assert "author" in str(exc_info.value)
+        with allure.step("Check that the ValidationError for 'author' value"):
+            errors = exc_info.value.errors()
+            assert errors[0]["loc"] == ("author",)
 
-@allure.feature("Test models")
-@allure.story("Create a new task with invalid 'description'")
-@allure.severity(allure.severity_level.NORMAL)
-def test_task_create_description_too_long():
-    with allure.step("Fill payload with too long description"):
+    @allure.title("Create a new task with invalid 'description'")
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_task_create_description_too_long(self):
         """Негативный тест: описание ровно 541 символ (граница > 540)"""
-        payload = {
-        "name": "Валидная задача",
-        "description": "D" * 541,
-        "author": "Tech Support"
-        }
+        with allure.step("Fill payload with too long description"):
+            payload = {
+            "name": "Валидная задача",
+            "description": "D" * 541,
+            "author": "Tech Support"
+            }
 
-    with allure.step("Check that the ValidationError is received"):
-        with pytest.raises(ValidationError) as exc_info:
-            TaskCreate(**payload)
+        with allure.step("Check that the ValidationError is received"):
+            with pytest.raises(ValidationError) as exc_info:
+                TaskCreate(**payload)
 
-    with allure.step("Check that the ValidationError for 'description' value"):
-        assert "description" in str(exc_info.value)
+        with allure.step("Check that the ValidationError for 'description' value"):
+            errors = exc_info.value.errors()
+            assert errors[0]["loc"] == ("description",)
